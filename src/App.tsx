ab76e653b0e374750b20199388a2b7260daeb057
riteshreg <riteshregmi85@gmail.com>
2024-05-21 15:25:10 +0545

    conversion_logic_optimized

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,11 @@ function App() {
   const [text, setText] = useState<string>("");
   const [nepaliWord, setNepaliWord] = useState<string>("");
 
-
-
   // useCallback to memoize the event handler
   const typeHandler = useCallback((event: KeyboardEvent) => {
     const { key } = event;
 
-
-    // clicking the backspace should remove the text; 
+    // clicking the backspace should remove the text;
     if (key === "Backspace") {
       setText((prevState) => prevState.slice(0, -1));
       return;
@@ -24,12 +21,10 @@ function App() {
       return;
     }
 
-
     // when some special character is clicked it open some brower features to remove it we are making the event to default
     if (key === "/" || key === "'") {
-      event.preventDefault()
+      event.preventDefault();
     }
-
 
     // return if  non - printable characters is press;
     if (key.length !== 1) return;
@@ -37,9 +32,7 @@ function App() {
     //if key is not available in letter mapping then returingin
     if (!lettersMapping[key]) return;
 
-
     setText((prevState) => prevState + key);
-
   }, []);
 
   useEffect(() => {
@@ -48,18 +41,12 @@ function App() {
   }, [typeHandler]);
 
   useEffect(() => {
-
-    setNepaliWord(
-      convertEnglishToNepali(text)
-    )
+    setNepaliWord(convertEnglishToNepali(text, nepaliWord));
   }, [text]);
-
-
 
   return (
     <div className="bg-primaryColor min-h-full flex flex-col">
       <h1>Welcome to Jhapa Type</h1>
-      {text}
       <div className="mx-10">in Np: {nepaliWord}</div>
     </div>
   );
--- src/libs/conversion.tsx
+++ src/libs/conversion.tsx
@@ -1,13 +1,46 @@
 import { lettersMapping } from "../data/mapping";
 
-export function convertEnglishToNepali(englishText:string) {
-    const wordArray = englishText.split(" ");
+export function convertEnglishToNepali(
+  englishText: string,
+  nepaliText: string,
+) {
+  // splitting englishText into a array of string;
+  const EnglishWordArray = englishText.split(" ");
 
-    const finalNepaliWord = wordArray.map((word: string) => {
-        const splittedLetter: string[] = word.split("");
-        const x: string[] = splittedLetter.map((letter) => lettersMapping[letter]);
-        return x.join("");
-    });
+  // splitting all the nepaliText
+  const NepaliWordArray = nepaliText.split(" ");
 
-    return (finalNepaliWord.join(" "));
+  // this is the last english word which we have to convert to nepali;
+  const nepaliword = EnglishWordArray[EnglishWordArray.length - 1]
+    .split("")
+    .map((letter) => lettersMapping[letter])
+    .join("");
+
+  if (EnglishWordArray.length === NepaliWordArray.length) {
+    /*
+        if the length of englishWordArray and NepaliWordArray matches that's mean we are writing in
+        same word so as we only converting the last english to to nepali so 
+        We have to remove the last element of NepaliWordArray and Append a latest converted 
+        Nepali One
+        */
+    NepaliWordArray.splice(-1);
+    NepaliWordArray.push(nepaliword);
+  } else if (EnglishWordArray.length > NepaliWordArray.length) {
+    /*
+  if length englishWordArray is geater then lenght of nepaliWordArray then 
+   we have a new word so appending the new item to NepaliWord Array
+*/
+    NepaliWordArray.push(nepaliword);
+  } else if (EnglishWordArray.length < NepaliWordArray.length) {
+    /**
+     * if length of EnglishWordArray is smaller than Lenght of NepaliWordArray that's
+     * mean we are clicking the backspace but this condition will not meet for first 
+     * backspace click as both lenght will be same. this condition is required because, 
+     * there will be last empty string  in NepaliWordArray after removing all the letter
+     *  so we are just removing the last empty string of NepaliWordArray.
+     */
+    NepaliWordArray.splice(-1);
+  }
+
+  return NepaliWordArray.join(" ");
 }
